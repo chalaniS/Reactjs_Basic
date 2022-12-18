da12063a5dbdcab0e885044128b0e045a5cab14b
Chalani Saumya Rajapaksha <chalasaumya@gmail.com>
2022-12-18 12:47:54 +0530

    Reactjs basics

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [counter, setCounter] = useState(0);
+
+  useEffect(()=>{ 
+    alert("You've changed the cpunter to " + counter)   
+
+  }, [counter]);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App">     
+
+    <button onClick={() => setCounter((prevCount) => (prevCount = prevCount -1))}>-</button>
+    <h1>{counter}</h1>
+    <button onClick={() => setCounter((prevCount) => (prevCount = prevCount +1))}>+</button>    
+
+
     </div>
   );
 }
